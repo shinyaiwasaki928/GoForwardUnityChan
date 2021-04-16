27dfb9970b05d78a551086cf71e826d1ea89e84c
shinyaiwasaki928 <79783991+shinyaiwasaki928@users.noreply.github.com>
2021-04-16 21:36:47 +0900

    Initial commit

---
+++ Assets/CubeSound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //キューブ同士の接触、キューブが地面についたとき
+        if (collision.gameObject.tag == "CubeTag" || collision.gameObject.tag == "GroundTag")
+        {
+            //効果音を鳴らす
+            GetComponent<AudioSource>().Play();
+        }
+
+        //UnityChan2Dが接触したら
+        if (collision.gameObject.tag == "UnityChan2D")
+        {
+            //効果音を鳴らさない
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+}
